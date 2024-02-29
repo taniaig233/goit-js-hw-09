536f16538ce8bf59802410a27004d431ec07548b
taniaig233 <145375407+taniaig233@users.noreply.github.com>
2024-02-29 18:30:11 +0200

    Updated 3

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,36 @@
-console.log(`Form`);
+const LOCAL_KEY = 'feedback-form-state';
+
+const formFeedback = document.querySelector('.feedback-form');
+
+formFeedback.addEventListener('input', onInputData);
+formFeedback.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+const { email, message } = formFeedback.elements;
+reloadPage();
+
+function onInputData(e) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  localStorage.removeItem(LOCAL_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
+}
+
